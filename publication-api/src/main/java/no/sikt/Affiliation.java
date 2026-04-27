fcccb1983bbbbd18d0eb135d7a5ebefef39e82fd
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-04-27 13:40:28 +0200

    fix: Use correct values for user fixtures

--- publication-api/src/main/java/no/sikt/Affiliation.java
+++ publication-api/src/main/java/no/sikt/Affiliation.java
@@ -3,10 +3,13 @@ package no.sikt;
 public enum Affiliation {
   UIB("https://api.e2e.nva.aws.unit.no/cristin/organization/184.0.0.0");
 
-  /* default */
-  final String value;
+  private final String value;
 
   Affiliation(String value) {
     this.value = value;
+  }
+
+  public String getValue() {
+    return value;
   }
 }
--- publication-api/src/main/java/no/sikt/UserFixtures.java
+++ publication-api/src/main/java/no/sikt/UserFixtures.java
@@ -2,45 +2,51 @@ package no.sikt;
 
 public final class UserFixtures {
   public static final User UIB_CREATOR =
-      new User(
-          "Creator UiB ApiTestUser",
-          "api-test-user-creator-uib@test.sikt.no",
-          "1862458@184.0.0.0",
-          Affiliation.UIB.value);
+      User.builder()
+          .withName("Creator UiB ApiTestUser")
+          .withUserId("api-test-user-creator-uib@test.sikt.no")
+          .withCristinId("1862458@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
+          .build();
 
   public static final User UIB_PUBLISHING_CURATOR =
-      UIB_CREATOR
-          .copy()
+      User.builder()
           .withName("Publishing Curator UiB ApiTestUser")
-          .withCristinId("api-test-user-publishing-curator-uib@test.sikt.no")
+          .withUserId("api-test-user-publishing-curator-uib@test.sikt.no")
+          .withCristinId("1862459@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
           .build();
 
   public static final User UIB_NVI_CURATOR =
-      UIB_CREATOR
-          .copy()
+      User.builder()
           .withName("NVI Curator UiB ApiTestUser")
-          .withCristinId("api-test-user-nvi-curator-uib@test.sikt.no")
+          .withUserId("api-test-user-nvi-curator-uib@test.sikt.no")
+          .withCristinId("1862460@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
           .build();
 
   public static final User UIB_SUPPORT_CURATOR =
-      UIB_CREATOR
-          .copy()
+      User.builder()
           .withName("Support Curator UiB ApiTestUser")
-          .withCristinId("api-test-user-support-curator-uib@test.sikt.no")
+          .withUserId("api-test-user-support-curator-uib@test.sikt.no")
+          .withCristinId("1862461@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
           .build();
 
   public static final User UIB_DOI_CURATOR =
-      UIB_CREATOR
-          .copy()
+      User.builder()
           .withName("Doi Curator UiB ApiTestUser")
-          .withCristinId("api-test-user-doi-curator-uib@test.sikt.no")
+          .withUserId("api-test-user-doi-curator-uib@test.sikt.no")
+          .withCristinId("1862462@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
           .build();
 
   public static final User UIB_EDITOR =
-      UIB_CREATOR
-          .copy()
+      User.builder()
           .withName("Editor UiB ApiTestUser")
-          .withCristinId("api-test-user-editor-uib@test.sikt.no")
+          .withUserId("api-test-user-editor-uib@test.sikt.no")
+          .withCristinId("1862463@184.0.0.0")
+          .withAffiliations(Affiliation.UIB)
           .build();
 
   private UserFixtures() {}
