631c7eb53efc8a2ed69e97ea65902e525289f96c
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-05-27 10:32:01 +0200

    Using Awaitility to wait for response from search

--- bibtex/src/test/java/no/sikt/nva/apitest/search/resources/bibtex/BibTexTest.java
+++ bibtex/src/test/java/no/sikt/nva/apitest/search/resources/bibtex/BibTexTest.java
@@ -1,6 +1,7 @@
 package no.sikt.nva.apitest.search.resources.bibtex;
 
 import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static no.sikt.Category.ACADEMIC_ARTICLE;
 import static no.sikt.Category.ACADEMIC_MONOGRAPH;
 import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_MONTH_SHORT_NAME;
@@ -9,6 +10,8 @@ import static no.sikt.nva.apitest.base.UserFixtures.UIB_CREATOR;
 import static no.sikt.nva.apitest.base.UserFixtures.UIB_PUBLISHING_CURATOR;
 import static no.sikt.nva.apitest.search.BibTexExpectationFixtures.EXPECTED_BIBTEX_ACADEMIC_ARTICLE;
 import static no.sikt.nva.apitest.search.BibTexExpectationFixtures.EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH;
+import static org.awaitility.Awaitility.with;
+import static org.awaitility.pollinterval.FibonacciPollInterval.fibonacci;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 
@@ -20,10 +23,12 @@ import java.util.stream.Stream;
 import no.sikt.Category;
 import no.sikt.nva.apitest.search.BibTexExpectation;
 import no.sikt.nva.apitest.search.SearchTestBase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@SuppressWarnings("PMD.UnitTestShouldIncludeAssert")
 class BibTexTest extends SearchTestBase {
 
   private static Stream<Arguments> publicationsInBibTexFormatProvider() {
@@ -34,6 +39,7 @@ class BibTexTest extends SearchTestBase {
 
   @ParameterizedTest
   @MethodSource("publicationsInBibTexFormatProvider")
+  @DisplayName("Search with content type 'text/x-bibtex' produces BibTeX export")
   void shouldReturnPublicationsInBibTexFormat(Category category, BibTexExpectation expectation) {
 
     RestAssured.registerParser("text/x-bibtex", Parser.TEXT);
@@ -45,39 +51,29 @@ class BibTexTest extends SearchTestBase {
         PUBLICATION_FACTORY.createPublishedPublication(
             UIB_CREATOR, title, category, List.of(UIB_CREATOR), UIB_PUBLISHING_CURATOR);
 
-    var indexed = false;
-    var count = 0;
+    with()
+        .pollInterval(fibonacci().with().unit(SECONDS))
+        .await()
+        .atMost(12, SECONDS)
+        .until(() -> !getResponseBody(titleUuid).isEmpty());
 
-    while (!indexed && count < 3) {
-      count++;
+    var responseBody = getResponseBody(titleUuid);
+    var allExpectations = buildAllExpectations(expectation, title, identifier);
 
-      var responseBody =
-          given()
-              .param("query", titleUuid)
-              .accept("text/x-bibtex")
-              .when()
-              .get("/search/resources")
-              .then()
-              .statusCode(200)
-              .contentType("text/x-bibtex")
-              .extract()
-              .asString();
+    allExpectations.forEach(expected -> assertTrue(responseBody.contains(expected)));
+  }
 
-      if (responseBody.isEmpty()) {
-        try {
-          Thread.sleep(2000);
-        } catch (InterruptedException e) { // NOPMD - intentionally ignored, wait for search index
-        }
-      } else {
-        indexed = true;
-
-        var allExpectations = buildAllExpectations(expectation, title, identifier);
-
-        allExpectations.forEach(expected -> assertTrue(responseBody.contains(expected)));
-      }
-    }
-
-    assertTrue(indexed, "Publication was not indexed after 3 attempts");
+  private String getResponseBody(String titleUuid) {
+    return given()
+        .param("query", titleUuid)
+        .accept("text/x-bibtex")
+        .when()
+        .get("/search/resources")
+        .then()
+        .statusCode(200)
+        .contentType("text/x-bibtex")
+        .extract()
+        .asString();
   }
 
   private List<String> buildAllExpectations(
