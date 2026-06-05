219301207e244e5a48deaaac723830f73aa1f350
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-06-05 08:07:16 +0200

    Adding test for BookChapter

--- bibtex/src/test/java/no/sikt/nva/apitest/search/resources/bibtex/BibTexTest.java
+++ bibtex/src/test/java/no/sikt/nva/apitest/search/resources/bibtex/BibTexTest.java
@@ -5,13 +5,10 @@ import java.util.UUID;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import java.util.stream.Stream;
 
-import org.assertj.core.api.SoftAssertions;
-import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
-import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import static org.awaitility.Awaitility.with;
 import static org.awaitility.pollinterval.FibonacciPollInterval.fibonacci;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import static org.junit.jupiter.params.provider.Arguments.argumentSet;
@@ -22,6 +19,7 @@ import static io.restassured.RestAssured.given;
 import io.restassured.parsing.Parser;
 import no.sikt.Category;
 import static no.sikt.Category.ACADEMIC_ARTICLE;
+import static no.sikt.Category.ACADEMIC_CHAPTER;
 import static no.sikt.Category.ACADEMIC_MONOGRAPH;
 import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_MONTH_SHORT_NAME;
 import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_YEAR;
@@ -29,19 +27,18 @@ import static no.sikt.nva.apitest.base.UserFixtures.UIB_CREATOR;
 import static no.sikt.nva.apitest.base.UserFixtures.UIB_PUBLISHING_CURATOR;
 import no.sikt.nva.apitest.search.BibTexExpectation;
 import static no.sikt.nva.apitest.search.BibTexExpectationFixtures.EXPECTED_BIBTEX_ACADEMIC_ARTICLE;
+import static no.sikt.nva.apitest.search.BibTexExpectationFixtures.EXPECTED_BIBTEX_ACADEMIC_CHAPTER;
 import static no.sikt.nva.apitest.search.BibTexExpectationFixtures.EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH;
 import no.sikt.nva.apitest.search.SearchTestBase;
 
 @SuppressWarnings("PMD.UnitTestShouldIncludeAssert")
-@ExtendWith(SoftAssertionsExtension.class)
 class BibTexTest extends SearchTestBase {
-
-  @InjectSoftAssertions private SoftAssertions softly;
 
   private static Stream<Arguments> publicationsInBibTexFormatProvider() {
     return Stream.of(
         argumentSet("AcademicArticle", ACADEMIC_ARTICLE, EXPECTED_BIBTEX_ACADEMIC_ARTICLE),
-        argumentSet("AcademicMonograph", ACADEMIC_MONOGRAPH, EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH));
+        argumentSet("AcademicMonograph", ACADEMIC_MONOGRAPH, EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH),
+        argumentSet("AcademicChapter", ACADEMIC_CHAPTER, EXPECTED_BIBTEX_ACADEMIC_CHAPTER));
   }
 
   @ParameterizedTest
@@ -52,11 +49,27 @@ class BibTexTest extends SearchTestBase {
     RestAssured.registerParser("text/x-bibtex", Parser.TEXT);
 
     var titleUuid = UUID.randomUUID().toString();
+    var anthologyUuid = UUID.randomUUID().toString();
 
     var title = "BibTex Integration test publication " + titleUuid;
+    var anthologyTitle = "BibTex integration test anthology " + anthologyUuid;
+    var anthologyIdentifier =
+        category == ACADEMIC_CHAPTER
+            ? PUBLICATION_FACTORY.createAnthologyForChapter(
+                UIB_CREATOR, anthologyTitle, UIB_PUBLISHING_CURATOR, List.of(UIB_CREATOR))
+            : "";
     var identifier =
-        PUBLICATION_FACTORY.createPublishedPublication(
-            UIB_CREATOR, title, category, List.of(UIB_CREATOR), UIB_PUBLISHING_CURATOR);
+        category == ACADEMIC_CHAPTER
+            ? PUBLICATION_FACTORY.createChapterInAnthology(
+                UIB_CREATOR,
+                title,
+                category,
+                List.of(UIB_CREATOR),
+                UIB_PUBLISHING_CURATOR,
+                anthologyIdentifier,
+                List.of(UIB_CREATOR))
+            : PUBLICATION_FACTORY.createPublishedPublication(
+                UIB_CREATOR, title, category, List.of(UIB_CREATOR), UIB_PUBLISHING_CURATOR);
 
     with()
         .pollInterval(fibonacci().with().unit(SECONDS))
@@ -67,7 +80,28 @@ class BibTexTest extends SearchTestBase {
     var responseBody = getResponseBody(titleUuid);
     var allExpectations = buildAllExpectations(expectation, title, identifier);
 
-    allExpectations.forEach(expected -> softly.assertThat(responseBody).contains(expected));
+    // if (category == ACADEMIC_CHAPTER) {
+    //   allExpectations =
+    //       Stream.concat(
+    //               allExpectations.stream(),
+    //               buildAllExpectations(
+    //                   EXPECTED_BIBTEX_BOOK_ANTHOLOGY, "Anthology for " + title,
+    // anthologyIdentifier)
+    //                   .stream())
+    //           .toList();
+    // }
+
+    // given()
+    // .param("query", titleUuid)
+    // .accept("text/x-bibtex")
+    // .when()
+    // .get("/search/resources")
+    // .then()
+    // .statusCode(200)
+    // .contentType("text/x-bibtex")
+    // .body("testing", Matchers.notNullValue());
+
+    allExpectations.forEach(expected -> assertTrue(responseBody.contains(expected)));
   }
 
   private String getResponseBody(String titleUuid) {
@@ -92,7 +126,8 @@ class BibTexTest extends SearchTestBase {
                 "url = {" + RestAssured.baseURI + "/publication/" + identifier + "}",
                 "title = {" + title + "}",
                 "month = {" + CURRENT_MONTH_SHORT_NAME + "}",
-                "year = {" + CURRENT_YEAR + "}"))
+                "year = {" + CURRENT_YEAR + "}",
+                "}"))
         .toList();
   }
 }
--- bibtex/src/testFixtures/java/no/sikt/nva/apitest/search/BibTexExpectationFixtures.java
+++ bibtex/src/testFixtures/java/no/sikt/nva/apitest/search/BibTexExpectationFixtures.java
@@ -4,22 +4,34 @@ import java.util.List;
 
 public class BibTexExpectationFixtures {
 
-  public static final BibTexExpectation EXPECTED_BIBTEX_ACADEMIC_ARTICLE =
-      new BibTexExpectation(
-          "article",
-          List.of(
-              "journal = {ACM Journal of Data and Information Quality}",
-              "issn = {1936-1963}",
-              "note = {nva type: AcademicArticle}",
-              "number = {1}",
-              "pages = {10--20}",
-              "volume = {3}"));
-  public static final BibTexExpectation EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH =
-      new BibTexExpectation(
-          "book",
-          List.of(
-              "isbn = {9783161484100}",
-              "note = {nva type: AcademicMonograph}",
-              "pages = {150}",
-              "publisher = {Springer Nature}"));
+  public static final BibTexExpectation EXPECTED_BIBTEX_ACADEMIC_ARTICLE = new BibTexExpectation(
+      "article",
+      List.of(
+          "journal = {ACM Journal of Data and Information Quality}",
+          "issn = {1936-1963}",
+          "note = {nva type: AcademicArticle}",
+          "number = {1}",
+          "pages = {10--20}",
+          "volume = {3}"));
+  public static final BibTexExpectation EXPECTED_BIBTEX_ACADEMIC_MONOGRAPH = new BibTexExpectation(
+      "book",
+      List.of(
+          "isbn = {9783161484100}",
+          "note = {nva type: AcademicMonograph}",
+          "pages = {150}",
+          "publisher = {Springer Nature}"));
+  public static final BibTexExpectation EXPECTED_BIBTEX_BOOK_ANTHOLOGY = new BibTexExpectation(
+      "book",
+      List.of(
+          "isbn = {9783161484100}",
+          "note = {nva type: BookAnthology}",
+          "pages = {150}",
+          "publisher = {Springer Nature}"));
+  public static final BibTexExpectation EXPECTED_BIBTEX_ACADEMIC_CHAPTER = new BibTexExpectation(
+      "inbook",
+      List.of(
+          "isbn = {9783161484100}",
+          "note = {nva type: AcademicChapter}",
+          "pages = {1--20}",
+          "publisher = {Springer Nature}"));
 }
