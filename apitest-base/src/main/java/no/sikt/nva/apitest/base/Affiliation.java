7d52619ba05ec1057d895ae3de7de1bd7351f1fb
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-08-07 07:21:14 +0200

    UserFixtures for nvi test users

--- apitest-base/src/main/java/no/sikt/nva/apitest/base/Affiliation.java
+++ apitest-base/src/main/java/no/sikt/nva/apitest/base/Affiliation.java
@@ -1,7 +1,10 @@
 package no.sikt.nva.apitest.base;
 
 public enum Affiliation {
-  UIB("https://api.e2e.nva.aws.unit.no/cristin/organization/184.0.0.0");
+  UIB("https://api.e2e.nva.aws.unit.no/cristin/organization/184.0.0.0"),
+  KRISTIANIA("https://api.e2e.nva.aws.unit.no/cristin/organization/1615.0.0.0"),
+  OSLO_MET("https://api.e2e.nva.aws.unit.no/cristin/organization/215.0.0.0"),
+  UIS("https://api.e2e.nva.aws.unit.no/cristin/organization/217.0.0.0");
 
   private final String value;
 
