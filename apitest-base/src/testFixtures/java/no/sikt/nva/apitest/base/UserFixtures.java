bcf49feb695411c536b84b1cd3e958b46f84f9af
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-08-07 08:05:39 +0200

    Added app-admin user to TestFixtures

--- apitest-base/src/testFixtures/java/no/sikt/nva/apitest/base/UserFixtures.java
+++ apitest-base/src/testFixtures/java/no/sikt/nva/apitest/base/UserFixtures.java
@@ -65,100 +65,12 @@ public final class UserFixtures {
           .withAffiliations(Affiliation.UIB)
           .build();
 
-  public static final User KRISTIANIA_CREATOR =
+  public static final User APP_ADMIN =
       User.builder()
-          .withName("Registrator Kristiania ApiTestUser")
-          .withUserId("api-test-user-registrator-kristiania@test.sikt.no")
-          .withCristinId("1862476@1615.0.0.0")
-          .withAffiliations(Affiliation.KRISTIANIA)
-          .build();
-
-  public static final User KRISTIANIA_PUBLISHING_CURATOR =
-      User.builder()
-          .withName("Publishing Curator Kristiania ApiTestUser")
-          .withUserId("api-test-user-publishing-curator-kristiania@test.sikt.no")
-          .withCristinId("1862477@1615.0.0.0")
-          .withAffiliations(Affiliation.KRISTIANIA)
-          .build();
-
-  public static final User KRISTIANIA_NVI_CURATOR =
-      User.builder()
-          .withName("Nvi-curator Kristiania ApiTestUser")
-          .withUserId("api-test-user-nvi-curator-kristiania@test.sikt.no")
-          .withCristinId("1862478@1615.0.0.0")
-          .withAffiliations(Affiliation.KRISTIANIA)
-          .build();
-
-  public static final User KRISTIANIA_EDITOR =
-      User.builder()
-          .withName("Editor Kristiania ApiTestUser")
-          .withUserId("api-test-user-editor-kristiania@test.sikt.no")
-          .withCristinId("1862479@1615.0.0.0")
-          .withAffiliations(Affiliation.KRISTIANIA)
-          .build();
-
-  public static final User OSLO_MET_CREATOR =
-      User.builder()
-          .withName("Registrator OsloMet ApiTestUser")
-          .withUserId("api-test-user-registrator-oslomet@test.sikt.no")
-          .withCristinId("1862480@215.0.0.0")
-          .withAffiliations(Affiliation.OSLO_MET)
-          .build();
-
-  public static final User OSLO_MET_PUBLISHING_CURATOR =
-      User.builder()
-          .withName("Publishing curator OsloMet ApiTestUser")
-          .withUserId("api-test-user-publishing-curator-oslomet@test.sikt.no")
-          .withCristinId("1862481@215.0.0.0")
-          .withAffiliations(Affiliation.OSLO_MET)
-          .build();
-
-  public static final User OSLO_MET_NVI_CURATOR =
-      User.builder()
-          .withName("Nvi-curator OsloMet ApiTestUser")
-          .withUserId("api-test-user-nvi-curator-oslomet@test.sikt.no")
-          .withCristinId("1862482@215.0.0.0")
-          .withAffiliations(Affiliation.OSLO_MET)
-          .build();
-
-  public static final User OSLO_MET_EDITOR =
-      User.builder()
-          .withName("Editor OsloMet ApiTestUser")
-          .withUserId("api-test-user-editor-oslomet@test.sikt.no")
-          .withCristinId("1862483@215.0.0.0")
-          .withAffiliations(Affiliation.OSLO_MET)
-          .build();
-
-  public static final User UIS_CREATOR =
-      User.builder()
-          .withName("Registrator UiS ApiTestUser")
-          .withUserId("api-test-user-registrator-uis@test.sikt.no")
-          .withCristinId("1862472@217.0.0.0")
-          .withAffiliations(Affiliation.UIS)
-          .build();
-
-  public static final User UIS_NVI_CURATOR =
-      User.builder()
-          .withName("Nvi-curator UiS ApiTestUser")
-          .withUserId("api-test-user-nvi-curator-uis@test.sikt.no")
-          .withCristinId("1862474@217.0.0.0")
-          .withAffiliations(Affiliation.UIS)
-          .build();
-
-  public static final User UIS_PUBLISHING_CURATOR =
-      User.builder()
-          .withName("Publishing Curator UiS ApiTestUser")
-          .withUserId("api-test-user-publishing-curator-uis@test.sikt.no")
-          .withCristinId("1862473@217.0.0.0")
-          .withAffiliations(Affiliation.UIS)
-          .build();
-
-  public static final User UIS_EDITOR =
-      User.builder()
-          .withName("Editor UiS ApiTestUser")
-          .withUserId("api-test-user-editor-uis@test.sikt.no")
-          .withCristinId("1862475@217.0.0.0")
-          .withAffiliations(Affiliation.UIS)
+          .withName("Create testdata TestUser")
+          .withUserId("admin-user-testdata@test.no")
+          .withCristinId("1862138@20754.0.0.0")
+          .withAffiliations(Affiliation.SIKT)
           .build();
 
   private UserFixtures() {}
