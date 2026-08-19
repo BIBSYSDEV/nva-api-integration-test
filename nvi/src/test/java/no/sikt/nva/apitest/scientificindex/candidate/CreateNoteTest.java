a47079e308b5f09e74977afd096c87a6c3e2dcb9
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-08-19 14:54:58 +0200

    fix: Add polling to avoid transient errors from conflicts

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/CreateNoteTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/CreateNoteTest.java
@@ -1,23 +1,29 @@
 package no.sikt.nva.apitest.scientificindex.candidate;
 
-import java.util.Map;
-import java.util.UUID;
+import static java.util.UUID.randomUUID;
+import static no.sikt.nva.apitest.base.Polling.pollUntil;
+import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedJsonRequestAsUser;
+import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_CREATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_NVI_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_PUBLISHING_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_NVI_CURATOR;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_NOTES_PATH;
 
+import io.qameta.allure.Description;
+import io.restassured.response.Response;
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import no.sikt.Contributor;
+import no.sikt.nva.apitest.base.User;
+import no.sikt.nva.apitest.scientificindex.NviCandidate;
+import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import io.qameta.allure.Description;
-import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedJsonRequestAsUser;
-import no.sikt.nva.apitest.base.User;
-import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_CREATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_NVI_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_NVI_CURATOR;
-import no.sikt.nva.apitest.scientificindex.NviCandidate;
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_NOTES_PATH;
-import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 
 @ExtendWith(SoftAssertionsExtension.class)
 @DisplayName("POST " + CANDIDATE_NOTES_PATH)
@@ -28,17 +34,12 @@ class CreateNoteTest extends ScientificIndexTestBase {
   @DisplayName("Create note on NVI candidate")
   @Description(useJavaDoc = true)
   void shouldCreateNote(SoftAssertions softly) {
-
-    var candidate = createCandidate(OSLO_MET_CREATOR);
-
+    var candidate = createCandidate();
     var candidateIdentifier = candidate.candidateIdentifier();
-    var payload = createNote();
+    var payload = createNoteRequest();
 
     var response =
-        givenAuthenticatedJsonRequestAsUser(OSLO_MET_NVI_CURATOR)
-            .body(payload)
-            .when()
-            .post(CANDIDATE_NOTES_PATH, candidateIdentifier)
+        givenCreateNoteRequest(OSLO_MET_NVI_CURATOR, candidateIdentifier, payload)
             .then()
             .statusCode(200)
             .extract()
@@ -55,18 +56,11 @@ class CreateNoteTest extends ScientificIndexTestBase {
   @DisplayName("Create note on NVI candidate when not NVI-curator for owner")
   @Description(useJavaDoc = true)
   void shouldReturnUnauthorizedWhenCreatingNoteOnCandidateNotOwned() {
-
-    var candidate = createCandidate(OSLO_MET_CREATOR);
-
+    var candidate = createCandidate();
     var candidateIdentifier = candidate.candidateIdentifier();
-    var payload = createNote();
+    var payload = createNoteRequest();
 
-    givenAuthenticatedJsonRequestAsUser(UIB_NVI_CURATOR)
-        .body(payload)
-        .when()
-        .post(CANDIDATE_NOTES_PATH, candidateIdentifier)
-        .then()
-        .statusCode(401);
+    givenCreateNoteRequest(UIB_NVI_CURATOR, candidateIdentifier, payload).then().statusCode(401);
   }
 
   /** Trying to create a note on a non-existing candidate returns {@code 404 Not Found} */
@@ -74,16 +68,10 @@ class CreateNoteTest extends ScientificIndexTestBase {
   @DisplayName("Create note on non-existing NVI candidate")
   @Description(useJavaDoc = true)
   void shouldReturnNotFoundWhenCreatingNoteOnNonExistingCandidate() {
+    var candidateIdentifier = randomUUID().toString();
+    var payload = createNoteRequest();
 
-    var candidateIdentifier = UUID.randomUUID().toString();
-    var payload = createNote();
-
-    givenAuthenticatedJsonRequestAsUser(UIB_NVI_CURATOR)
-        .body(payload)
-        .when()
-        .post(CANDIDATE_NOTES_PATH, candidateIdentifier)
-        .then()
-        .statusCode(404);
+    givenCreateNoteRequest(UIB_NVI_CURATOR, candidateIdentifier, payload).then().statusCode(404);
   }
 
   /** Creating a note with no payload returns {@code 400 Bad Request} */
@@ -91,9 +79,7 @@ class CreateNoteTest extends ScientificIndexTestBase {
   @DisplayName("Create empty note")
   @Description(useJavaDoc = true)
   void shouldReturnInvalidRequestBodyWhenCreatingEmptyNote(SoftAssertions softly) {
-
-    var candidate = createCandidate(OSLO_MET_CREATOR);
-
+    var candidate = createCandidate();
     var candidateIdentifier = candidate.candidateIdentifier();
 
     var response =
@@ -113,17 +99,12 @@ class CreateNoteTest extends ScientificIndexTestBase {
   @DisplayName("Create note with wrong payload")
   @Description(useJavaDoc = true)
   void shouldReturnBadRequestWhenCreatingNoteWithWrongPayload(SoftAssertions softly) {
-
-    var candidate = createCandidate(OSLO_MET_CREATOR);
-
+    var candidate = createCandidate();
     var candidateIdentifier = candidate.candidateIdentifier();
-    var payload = Map.of("noteText", "NVI integration test " + UUID.randomUUID());
+    var payload = Map.of("noteText", "NVI integration test " + randomUUID());
 
     var response =
-        givenAuthenticatedJsonRequestAsUser(OSLO_MET_NVI_CURATOR)
-            .body(payload)
-            .when()
-            .post(CANDIDATE_NOTES_PATH, candidateIdentifier)
+        givenCreateNoteRequest(OSLO_MET_NVI_CURATOR, candidateIdentifier, payload)
             .then()
             .statusCode(400)
             .extract()
@@ -134,12 +115,32 @@ class CreateNoteTest extends ScientificIndexTestBase {
         .isEqualTo("Request body must contain text field.");
   }
 
-  private NviCandidate createCandidate(User user) {
-    return CANDIDATE_FACTORY.createCandidate(
-        "NVI integration test candidate with note " + UUID.randomUUID(), user);
+  private NviCandidate createCandidate() {
+    var title = "NVI - Report status test - %s".formatted(randomUUID());
+    var contributors = List.of(Contributor.asCreator(OSLO_MET_CREATOR));
+    return CANDIDATE_FACTORY.createCandidate(title, OSLO_MET_PUBLISHING_CURATOR, contributors);
   }
 
-  private Map<String, String> createNote() {
-    return Map.of("text", "NVI integration test " + UUID.randomUUID());
+  private static Response givenCreateNoteRequest(
+      User user, String candidateIdentifier, Map<String, String> requestBody) {
+    return pollUntil(
+        postCreateNoteRequest(user, candidateIdentifier, requestBody),
+        CreateNoteTest::isNotConflict);
+  }
+
+  private static Map<String, String> createNoteRequest() {
+    return Map.of("text", "NVI integration test " + randomUUID());
+  }
+
+  private static Callable<Response> postCreateNoteRequest(
+      User user, String candidateIdentifier, Map<String, String> requestBody) {
+    return () ->
+        givenAuthenticatedJsonRequestAsUser(user)
+            .body(requestBody)
+            .post(CANDIDATE_NOTES_PATH, candidateIdentifier);
+  }
+
+  private static boolean isNotConflict(Response response) {
+    return response.statusCode() != HttpURLConnection.HTTP_CONFLICT;
   }
 }
