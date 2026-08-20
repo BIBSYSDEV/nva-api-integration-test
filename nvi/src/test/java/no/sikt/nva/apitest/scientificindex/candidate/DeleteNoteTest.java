845d13181059b7c55216875765013e3358aa2984
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-08-20 11:20:14 +0200

    refactoring

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/DeleteNoteTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/DeleteNoteTest.java
@@ -1,26 +1,8 @@
 package no.sikt.nva.apitest.scientificindex.candidate;
 
-import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
-import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_CREATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_NVI_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_CREATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_DOI_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_EDITOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_PUBLISHING_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_SUPPORT_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIS_NVI_CURATOR;
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_NOTES_DELETE_PATH;
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_PATH;
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.DELETE_NOTE_PATH;
-import static org.junit.jupiter.params.provider.Arguments.argumentSet;
-
-import io.qameta.allure.Description;
-import io.restassured.path.json.JsonPath;
 import java.util.UUID;
 import java.util.stream.Stream;
-import no.sikt.nva.apitest.base.User;
-import no.sikt.nva.apitest.scientificindex.NviCandidate;
-import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
+
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.Disabled;
@@ -29,7 +11,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import io.qameta.allure.Description;
+import io.restassured.path.json.JsonPath;
+import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
+import no.sikt.nva.apitest.base.User;
+import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_CREATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.OSLO_MET_NVI_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_CREATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_DOI_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_EDITOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_PUBLISHING_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIB_SUPPORT_CURATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIS_NVI_CURATOR;
+import no.sikt.nva.apitest.scientificindex.NviCandidate;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_NOTES_DELETE_PATH;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_PATH;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.DELETE_NOTE_PATH;
+import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 
 @ExtendWith(SoftAssertionsExtension.class)
 @DisplayName("DELETE " + DELETE_NOTE_PATH)
@@ -123,7 +124,7 @@ class DeleteNoteTest extends ScientificIndexTestBase {
 
   /** Trying to delete a non-existing note from a NVI candidate returns {@code 404 Not Found} */
   @Test
-  @Disabled("Bug: See NP-51616")
+  @Disabled("FIXME: Returns 502, should return 505. See NP-51616")
   @DisplayName("Delete non-existing note from candidate returns not found")
   @Description(useJavaDoc = true)
   void shouldReturnNotFoundWhenDeletingNonExistingNote() {
