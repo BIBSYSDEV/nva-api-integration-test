28e5f362ae0663c88385acaf2a26f11598d22148
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-08-20 08:31:40 +0200

    refactor: Move user providers to base class

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/FetchCandidateByPublicationTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/FetchCandidateByPublicationTest.java
@@ -3,19 +3,10 @@ package no.sikt.nva.apitest.scientificindex.candidate;
 import static java.util.UUID.randomUUID;
 import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedJsonRequestAsUser;
 import static no.sikt.nva.apitest.base.Requests.givenUnauthenticatedJsonRequest;
-import static no.sikt.nva.apitest.base.UserFixtures.APP_ADMIN;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_CREATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_DOI_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_EDITOR;
 import static no.sikt.nva.apitest.base.UserFixtures.UIB_NVI_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_PUBLISHING_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_SUPPORT_CURATOR;
-import static no.sikt.nva.apitest.base.UserFixtures.UIB_THESIS_CURATOR;
 import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_BY_PUBLICATION_PATH;
-import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 
 import io.qameta.allure.Description;
-import java.util.stream.Stream;
 import no.sikt.nva.apitest.base.User;
 import no.sikt.nva.apitest.scientificindex.NviCandidate;
 import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
@@ -27,7 +18,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @ExtendWith(SoftAssertionsExtension.class)
@@ -56,7 +46,7 @@ class FetchCandidateByPublicationTest extends ScientificIndexTestBase {
   /** Fetching a candidate as a non-NVI user returns status {@code 403 Forbidden}. */
   @ParameterizedTest
   @Disabled("FIXME: Returns 401, but should be 403. See NP-51618.")
-  @MethodSource("nonNviUserProvider")
+  @MethodSource("usersWithoutNviAccess")
   @DisplayName("Fetch candidate unauthorized")
   @Description(useJavaDoc = true)
   void shouldReturnUnauthorizedWhenFetchingCandidateWithoutAccess(User user) {
@@ -64,16 +54,6 @@ class FetchCandidateByPublicationTest extends ScientificIndexTestBase {
         .get(CANDIDATE_BY_PUBLICATION_PATH, candidate.publicationIdentifier())
         .then()
         .statusCode(403);
-  }
-
-  private static Stream<Arguments> nonNviUserProvider() {
-    return Stream.of(
-        argumentSet("Editor", UIB_EDITOR),
-        argumentSet("Registrar", UIB_CREATOR),
-        argumentSet("DOI curator", UIB_DOI_CURATOR),
-        argumentSet("Publishing curator", UIB_PUBLISHING_CURATOR),
-        argumentSet("Support curator", UIB_SUPPORT_CURATOR),
-        argumentSet("Thesis curator", UIB_THESIS_CURATOR));
   }
 
   /** Fetching a candidate for a non-candidate publication returns status {@code 404 Not Found}. */
@@ -89,7 +69,7 @@ class FetchCandidateByPublicationTest extends ScientificIndexTestBase {
 
   /** Fetching a candidate by its publication identifier returns it with status {@code 200 OK}. */
   @ParameterizedTest
-  @MethodSource("nviUserProvider")
+  @MethodSource("usersWithNviReadAccess")
   @DisplayName("Fetch candidate as NVI user")
   @Description(useJavaDoc = true)
   void shouldReturnCandidateWhenFetchingByCandidateIdentifier(User user, SoftAssertions softly) {
@@ -103,11 +83,5 @@ class FetchCandidateByPublicationTest extends ScientificIndexTestBase {
 
     softly.assertThat(response.getString("identifier")).isEqualTo(candidate.candidateIdentifier());
     softly.assertThat(response.getString("publicationId")).isEqualTo(candidate.publicationId());
-  }
-
-  private static Stream<Arguments> nviUserProvider() {
-    return Stream.of(
-        argumentSet("Application administrator", APP_ADMIN),
-        argumentSet("NVI curator", UIB_NVI_CURATOR));
   }
 }
