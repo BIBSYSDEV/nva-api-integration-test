36d26c740cb112d12553cb4462c0c570145763cf
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-08-24 08:29:47 +0200

    New test for assignee

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/UpdateCandidateAssigneeTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/candidate/UpdateCandidateAssigneeTest.java
@@ -1,25 +1,44 @@
 package no.sikt.nva.apitest.scientificindex.candidate;
 
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_ASSIGNEE_PATH;
+import java.util.List;
+import java.util.UUID;
 
-import io.qameta.allure.Description;
-import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import io.qameta.allure.Description;
+import no.sikt.Contributor;
+import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
+import static no.sikt.nva.apitest.base.UserFixtures.UIS_CREATOR;
+import static no.sikt.nva.apitest.base.UserFixtures.UIS_NVI_CURATOR;
+import no.sikt.nva.apitest.scientificindex.NviCandidate;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.CANDIDATE_ASSIGNEE_PATH;
+import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
+
 @ExtendWith(SoftAssertionsExtension.class)
 @DisplayName("PUT " + CANDIDATE_ASSIGNEE_PATH)
 class UpdateCandidateAssigneeTest extends ScientificIndexTestBase {
 
-  // TODO: Add tests
-
   @Test
-  @DisplayName("placeholder")
+  @DisplayName("Assign curator to candidate")
   @Description(useJavaDoc = true)
-  void placeholder(SoftAssertions softly) {
-    softly.assertThat(CANDIDATE_ASSIGNEE_PATH).isNotBlank();
+  void shouldAssignCuratorToCandidate(SoftAssertions softly) {
+
+    var candidate = createCandidate();
+    var candidateIdentifier = candidate.candidateIdentifier();
+
+    givenAuthenticatedRequestAsUser(UIS_NVI_CURATOR)
+    .when()
+    .put(CANDIDATE_ASSIGNEE_PATH, candidateIdentifier)
+    .then()
+    .statusCode(200);
+  }
+
+  private NviCandidate createCandidate() {
+    var title = "Test NVI canidate " + UUID.randomUUID();
+    return CANDIDATE_FACTORY.createCandidate(title, UIS_NVI_CURATOR, List.of(Contributor.asCreator(UIS_CREATOR)));
   }
 }
