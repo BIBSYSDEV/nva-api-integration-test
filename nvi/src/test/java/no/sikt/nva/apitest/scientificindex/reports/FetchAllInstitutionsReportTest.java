befbb909db57c5903f993ee0e129e5c892789709
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-08-26 11:34:05 +0200

    Init test

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/reports/FetchAllInstitutionsReportTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/reports/FetchAllInstitutionsReportTest.java
@@ -1,25 +1,47 @@
 package no.sikt.nva.apitest.scientificindex.reports;
 
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.INSTITUTION_REPORTS_PATH;
+import java.util.List;
 
-import io.qameta.allure.Description;
-import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import io.qameta.allure.Description;
+import static no.sikt.nva.apitest.base.Affiliation.KRISTIANIA;
+import static no.sikt.nva.apitest.base.Affiliation.OSLO_MET;
+import static no.sikt.nva.apitest.base.Affiliation.UIB;
+import static no.sikt.nva.apitest.base.Affiliation.UIS;
+import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_YEAR;
+import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
+import no.sikt.nva.apitest.base.UserFixtures;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.INSTITUTION_REPORTS_PATH;
+import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
+
 @ExtendWith(SoftAssertionsExtension.class)
 @DisplayName("GET " + INSTITUTION_REPORTS_PATH)
 class FetchAllInstitutionsReportTest extends ScientificIndexTestBase {
 
-  // TODO: Add tests
-
   @Test
-  @DisplayName("placeholder")
+  @DisplayName("Fetch report for all institutions")
   @Description(useJavaDoc = true)
-  void placeholder(SoftAssertions softly) {
-    softly.assertThat(INSTITUTION_REPORTS_PATH).isNotBlank();
+  void shouldReturnReportFromAllInstitutionsForCurrentYear(SoftAssertions softly) {
+    var response = givenAuthenticatedRequestAsUser(UserFixtures.UIS_NVI_CURATOR)
+    .when()
+    .get(INSTITUTION_REPORTS_PATH, CURRENT_YEAR)
+    .then()
+    .statusCode(200).extract().response();
+
+    var affiliations = List.of(
+      UIB.getValue(),
+      UIS.getValue(),
+      KRISTIANIA.getValue(),
+      OSLO_MET.getValue()
+    );
+
+    var jsonPath = response.jsonPath().para("affiliation", affiliation);
+
+    softly.assertThat(response.jsonPath().getString("type")).isEqualTo("AllInstitutionsReport");
   }
 }
