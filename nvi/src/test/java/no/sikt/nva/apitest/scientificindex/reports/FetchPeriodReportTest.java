35a754afe04b610b10af887ec22aacb91ba33354
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-09-01 09:48:55 +0200

    Formatting

--- nvi/src/test/java/no/sikt/nva/apitest/scientificindex/reports/FetchPeriodReportTest.java
+++ nvi/src/test/java/no/sikt/nva/apitest/scientificindex/reports/FetchPeriodReportTest.java
@@ -4,7 +4,19 @@ import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
+import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_YEAR;
+import static no.sikt.nva.apitest.base.CurrentTimeConstants.getCurrentYear;
+import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
+import static no.sikt.nva.apitest.base.Requests.givenUnauthenticatedJsonRequest;
+import static no.sikt.nva.apitest.base.UserFixtures.UIS_NVI_CURATOR;
+import static no.sikt.nva.apitest.scientificindex.NviReports.Periods.THIS_PERIOD;
+import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.PERIOD_REPORT_PATH;
 
+import io.qameta.allure.Description;
+import io.restassured.response.ValidatableResponse;
+import no.sikt.nva.apitest.base.User;
+import no.sikt.nva.apitest.scientificindex.NviReports;
+import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.DisplayName;
@@ -12,19 +24,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import io.qameta.allure.Description;
-import io.restassured.response.ValidatableResponse;
-import static no.sikt.nva.apitest.base.CurrentTimeConstants.CURRENT_YEAR;
-import static no.sikt.nva.apitest.base.CurrentTimeConstants.getCurrentYear;
-import static no.sikt.nva.apitest.base.Requests.givenAuthenticatedRequestAsUser;
-import static no.sikt.nva.apitest.base.Requests.givenUnauthenticatedJsonRequest;
-import no.sikt.nva.apitest.base.User;
-import static no.sikt.nva.apitest.base.UserFixtures.UIS_NVI_CURATOR;
-import no.sikt.nva.apitest.scientificindex.NviReports;
-import static no.sikt.nva.apitest.scientificindex.NviReports.Periods.THIS_PERIOD;
-import static no.sikt.nva.apitest.scientificindex.ScientificIndexPaths.PERIOD_REPORT_PATH;
-import no.sikt.nva.apitest.scientificindex.ScientificIndexTestBase;
 
 @ExtendWith(SoftAssertionsExtension.class)
 @DisplayName("GET " + PERIOD_REPORT_PATH)
