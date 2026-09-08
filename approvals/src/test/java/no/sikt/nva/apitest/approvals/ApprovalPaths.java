869160952670cc769bdae1f279d9d5ced08c211d
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-09-08 12:26:09 +0200

    test(approvals): test fetching an approval by identifier #NP-51651

--- approvals/src/test/java/no/sikt/nva/apitest/approvals/ApprovalPaths.java
+++ approvals/src/test/java/no/sikt/nva/apitest/approvals/ApprovalPaths.java
@@ -6,6 +6,7 @@ public final class ApprovalPaths {
 
   public static final String CONTEXT_PATH = BASE_PATH + "/context";
   public static final String ONTOLOGY_PATH = BASE_PATH + "/ontology";
+  public static final String APPROVAL_PATH = BASE_PATH + "/{approvalId}";
 
   private ApprovalPaths() {}
 }
