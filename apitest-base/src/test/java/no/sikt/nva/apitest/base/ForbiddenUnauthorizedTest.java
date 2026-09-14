878e22ae9537ef676ea32a6e169a439f32b6fd1e
Eirik Nilsen <eirik.nilsen@sikt.no>
2026-09-14 13:39:03 +0200

    Publication api

--- apitest-base/src/test/java/no/sikt/nva/apitest/base/ForbiddenUnauthorizedTest.java
+++ apitest-base/src/test/java/no/sikt/nva/apitest/base/ForbiddenUnauthorizedTest.java
@@ -28,26 +28,67 @@ class ForbiddenUnauthorizedTest extends IntegrationTestBase {
 
   private static final String PRIMARY_ID = "123456";
   private static final String SECONDARY_ID = "654321";
+  private static final String TERTIARY_ID = "321456";
 
   static Stream<Arguments> endpoints() {
     return Stream.of(
+      
+      argumentSet("POST /publication/{publicationIdentifier}/publish", POST, "/publication/{publicationIdentifier}/publish", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/file-upload/listparts", POST, "/publication/{publicationIdentifier}/file-upload/listparts", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/doi", POST, "/publication/{publicationIdentifier}/doi", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/file-upload/complete", POST, "/publication/{publicationIdentifier}/file-upload/complete", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/file/{fileIdentifier}", POST, "POST /publication/{publicationIdentifier}/file/{fileIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      argumentSet("DELETE /publication/{publicationIdentifier}/file/{fileIdentifier}", DELETE, "/publication/{publicationIdentifier}/file/{fileIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/ticket", POST, "/publication/{publicationIdentifier}/ticket", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("GET /publication/{publicationIdentifier}/log", GET, "/publication/{publicationIdentifier}/log", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("GET /publication/{publicationIdentifier}/tickets", GET, "/publication/{publicationIdentifier}/tickets", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/ticket/{ticketIdentifier}/message", POST, "/publication/{publicationIdentifier}/ticket/{ticketIdentifier}/message", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      argumentSet("POST /publication/", POST, "/publication/", UIB_CREATOR),
+      argumentSet("POST /publication/{publicationIdentifier}/file-upload/create", POST, "/publication/{publicationIdentifier}/file-upload/create", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("PUT /publication/{publicationIdentifier}", PUT, "/publication/{publicationIdentifier}", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("DELETE /publication/{publicationIdentifier}", DELETE, "/publication/{publicationIdentifier}", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("POST /publication/{publicationIdentifier}/file-upload/prepare", POST, "/publication/{publicationIdentifier}/file-upload/prepare", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("DELETE /publication/{publicationIdentifier}/ticket/{ticketIdentifier}/message/{messageIdentifier}", DELETE, "/publication/{publicationIdentifier}/ticket/{ticketIdentifier}/message/{messageIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID, TERTIARY_ID),
+      argumentSet("GET /publication/by-owner", GET, "/publication/by-owner", UIB_CREATOR),
+      argumentSet("POST /publication/{publicationIdentifier}/file-upload/abort", POST, "/publication/{publicationIdentifier}/file-upload/abort", UIB_CREATOR, PRIMARY_ID),
+      argumentSet("GET /publication/{publicationIdentifier}/ticket/{ticketIdentifier}", GET, "/publication/{publicationIdentifier}/ticket/{ticketIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      argumentSet("PUT /publication/{publicationIdentifier}/ticket/{ticketIdentifier}", PUT, "/publication/{publicationIdentifier}/ticket/{ticketIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      argumentSet("DELETE /publication/{publicationIdentifier}/ticket/{ticketIdentifier}", DELETE, "/publication/{publicationIdentifier}/ticket/{ticketIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID)
+
+      // argumentSet("GET /search/customer/import-candidates", GET, "/search/customer/import-candidates", UIB_CREATOR),
+      // argumentSet("GET /search/customer/resources", GET, "/search/customer/resources", UIB_CREATOR),
+      // argumentSet("GET /search/customer/tickets", POST, "/search/customer/tickets", UIB_CREATOR),
+      // argumentSet("GET /search/user/resources", POST, "/handle/", UIB_CREATOR)
+
+      // argumentSet("PUT /person-preferences/{cristinId+}", PUT, "/person-preferences/{cristinId}", UIB_CREATOR, PRIMARY_ID),
+
+      // argumentSet("POST /handle/", POST, "/handle/", UIB_CREATOR),
+      // argumentSet("PUT /handle/{prefix}/{suffix}", PUT, "/handle/{prefix}/{suffix}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID)
+
+      // argumentSet("POST /doi-fetch", POST, "/doi-fetch", UIB_CREATOR),
+      // argumentSet("POST /doi-fetch/preview/", POST, "/doi-fetch/preview/", UIB_CREATOR),
+      // argumentSet("POST /doi-registrar/draft/{doiPrefix}/{doiSuffix}", POST, "/doi-registrar/draft/{doiPrefix}/{doiSuffix}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      // argumentSet("DELETE /doi-registrar/draft/{doiPrefix}/{doiSuffix}", DELETE, "/doi-registrar/draft/{doiPrefix}/{doiSuffix}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      // argumentSet("POST /doi-registrar/findable", POST, "/doi-registrar/findable", UIB_CREATOR)
+
       // argumentSet("POST /customer", POST, "/customer", UIB_CREATOR),
-      argumentSet("GET /customer/{identifier}", GET, "/customer/{identifier}", UIB_CREATOR, SECONDARY_ID),
-      argumentSet("PUT /customer/{identifier}", PUT, "/customer/{identifier}", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("GET /customer/{identifier}/doiagent", GET, "/customer/{identifier}/doiagent", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("PUT /customer/{identifier}/doiagent", PUT, "/customer/{identifier}/doiagent", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("GET /customer/{identifier}/vocabularies", GET, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("POST /customer/{identifier}/vocabularies", POST, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("PUT /customer/{identifier}/vocabularies", PUT, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("POST /customer/{identifier}/channel-claim", POST, "/customer/{identifier}/channel-claim", UIB_CREATOR, SECONDARY_ID),
-      // argumentSet("DELETE /customer/{identifier}/channel-claim/{claimIdentifier}", DELETE, "/customer/{identifier}/channel-claim/{claimIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
-      argumentSet("GET /customer/channel-claims", GET, "/customer/channel-claims", UIB_CREATOR),
-      argumentSet("GET /customer/orgDomain/{orgDomain}", GET, "/customer/orgDomain/{orgDomain}", UIB_CREATOR, SECONDARY_ID),
-      argumentSet("GET /customer/cristinId/{cristinId}", GET, "/customer/cristinId/{cristinId}", UIB_CREATOR, PRIMARY_ID),
-      // argumentSet("POST /cristin/project", POST, "/cristin/project", UIB_CREATOR),
-      // argumentSet("POST /cristin/keywords", POST, "/cristin/keywords", UIB_CREATOR),
-      argumentSet("POST /cristin/person", POST, "/cristin/person", UIB_CREATOR),
-      argumentSet("PATCH /cristin/person/{id}", PATCH, "/cristin/person/{id}", UIB_CREATOR, PRIMARY_ID),
+      // argumentSet("GET /customer/{identifier}", GET, "/customer/{identifier}", UIB_CREATOR, SECONDARY_ID),
+      // argumentSet("PUT /customer/{identifier}", PUT, "/customer/{identifier}", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("GET /customer/{identifier}/doiagent", GET, "/customer/{identifier}/doiagent", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("PUT /customer/{identifier}/doiagent", PUT, "/customer/{identifier}/doiagent", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("GET /customer/{identifier}/vocabularies", GET, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("POST /customer/{identifier}/vocabularies", POST, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("PUT /customer/{identifier}/vocabularies", PUT, "/customer/{identifier}/vocabularies", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("POST /customer/{identifier}/channel-claim", POST, "/customer/{identifier}/channel-claim", UIB_CREATOR, SECONDARY_ID),
+      // // argumentSet("DELETE /customer/{identifier}/channel-claim/{claimIdentifier}", DELETE, "/customer/{identifier}/channel-claim/{claimIdentifier}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
+      // argumentSet("GET /customer/channel-claims", GET, "/customer/channel-claims", UIB_CREATOR),
+      // argumentSet("GET /customer/orgDomain/{orgDomain}", GET, "/customer/orgDomain/{orgDomain}", UIB_CREATOR, SECONDARY_ID),
+      // argumentSet("GET /customer/cristinId/{cristinId}", GET, "/customer/cristinId/{cristinId}", UIB_CREATOR, PRIMARY_ID),
+
+      // // argumentSet("POST /cristin/project", POST, "/cristin/project", UIB_CREATOR),
+      // // argumentSet("POST /cristin/keywords", POST, "/cristin/keywords", UIB_CREATOR),
+      // argumentSet("POST /cristin/person", POST, "/cristin/person", UIB_CREATOR),
+      // argumentSet("PATCH /cristin/person/{id}", PATCH, "/cristin/person/{id}", UIB_CREATOR, PRIMARY_ID),
       // argumentSet("POST /cristin/person/identityNumber", POST, "/cristin/person/identityNumber", UIB_CREATOR),
       // argumentSet("GET /cristin/person/{id}/organization/{orgId}", GET, "/cristin/person/{id}/organization/{orgId}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
       // argumentSet("PATCH /cristin/person/{id}/organization/{orgId}", PATCH, "/cristin/person/{id}/organization/{orgId}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
@@ -56,7 +97,8 @@ class ForbiddenUnauthorizedTest extends IntegrationTestBase {
       // argumentSet("DELETE /cristin/person/{id}/employment/{employmentId}", DELETE, "/cristin/person/{id}/employment/{employmentId}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
       // argumentSet("PATCH /cristin/person/{id}/employment/{employmentId}", PATCH, "/cristin/person/{id}/employment/{employmentId}", UIB_CREATOR, PRIMARY_ID, SECONDARY_ID),
       // argumentSet("PUT /cristin/person/{id}/picture", PUT, "/cristin/person/{id}/picture", UIB_CREATOR, PRIMARY_ID),
-      argumentSet("POST /approval", POST, "/approval", UIB_CREATOR)
+
+      // argumentSet("POST /approval", POST, "/approval", UIB_CREATOR)
       // argumentSet("POST /approval/{approvalId}", POST, "/approval/{approvalId}", UIB_CREATOR, SECONDARY_ID)
     );
   }
