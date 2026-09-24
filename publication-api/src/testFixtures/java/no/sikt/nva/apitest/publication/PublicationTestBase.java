a27ca7648266f821660cbf9184f69112c65b2107
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-09-24 12:31:08 +0200

    test: Add placeholder tests for remaining update endpoints

--- publication-api/src/testFixtures/java/no/sikt/nva/apitest/publication/PublicationTestBase.java
+++ publication-api/src/testFixtures/java/no/sikt/nva/apitest/publication/PublicationTestBase.java
@@ -1,5 +1,6 @@
 package no.sikt.nva.apitest.publication;
 
+import static java.util.UUID.randomUUID;
 import static no.sikt.nva.apitest.publication.PublicationFields.IDENTIFIER_FIELD;
 
 import no.sikt.nva.PublicationFactory;
@@ -15,5 +16,9 @@ public class PublicationTestBase extends IntegrationTestBase {
         .createDraftPublication(UserFixtures.UIB_CREATOR)
         .jsonPath()
         .getString(IDENTIFIER_FIELD);
+  }
+
+  public static String randomTitle() {
+    return "Test publication - %s".formatted(randomUUID());
   }
 }
